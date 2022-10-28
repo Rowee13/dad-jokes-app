40e95c3e2ce5386e960915726036676f760dd19e
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-10-29 02:19:32 +0800

    Added style on votes and setup localStorage

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -12,10 +12,16 @@ class JokeList extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = { jokes: [] };
+		this.state = {
+			jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
+		};
 	}
 
-	async componentDidMount() {
+	componentDidMount() {
+		if (this.state.jokes.length === 0) this.getJokes();
+	}
+
+	async getJokes() {
 		let jokes = [];
 		while (jokes.length < this.props.numJokesToGet) {
 			let res = await axios.get("https://icanhazdadjoke.com/", {
@@ -24,6 +30,7 @@ class JokeList extends Component {
 			jokes.push({ id: uuidv4(), text: res.data.joke, votes: 0 });
 		}
 		this.setState({ jokes: jokes });
+		window.localStorage.setItem("jokes", JSON.stringify(jokes));
 	}
 
 	handleVote(id, delta) {
